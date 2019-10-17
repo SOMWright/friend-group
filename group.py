13d7fd52ef8fb1f79253e9c107ffae0380dde683
Sam Wright <SOMWright@users.noreply.github.com>
2019-10-17 11:36:19 +0100

    Input group data

--- group.py
+++ group.py
@@ -2,4 +2,8 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {'Jill': {'Age': 26, 'Friends': ['Zalika'], 'Partner': ['John']},
+            'Zalika': {'Age': 28, 'Occupation': 'artist', 'Friends': ['Jill'], 'Landlords': ['Nash']},
+            'John': {'Age': 27, 'Occupation': 'writer', 'Partner': ['Jill']},
+            'Nash': {'Age': 34, 'Occupation': 'chef', 'Cousins': ['John'], 'Tenants': ['Zalika']}
+            }
